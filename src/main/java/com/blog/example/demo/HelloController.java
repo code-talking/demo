fa9db25ed39713e03341bd64319b4f5030ab2660
updatinglb <updatinglb@sina.com>
2019-11-04 20:33:35 -0800

    Initial commit

---
+++ src/main/java/com/blog/example/demo/HelloController.java
@@ -0,0 +1,35 @@
+package com.blog.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User:   bl
+ * Date:   2019-11-04
+ * Time:   20:19
+ * Package com.blog.example.demo
+ * File    HelloController
+ * Description
+ */
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String helloWorld(){
+        String msg = "hello world!";
+        return msg;
+    }
+
+    @RequestMapping("/blog")
+    public String blog() {
+        String msg = "Start to learn blog";
+        return msg;
+    }
+
+    @RequestMapping("/users")
+    public String user() {
+        String msg = "users";
+        return msg;
+    }
+}
